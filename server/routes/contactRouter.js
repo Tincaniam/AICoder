a2a6670a5b3ad7f23218cc686bfe7f4f5e55bf49
Piseth <chhoeuyp@oregonstate.edu>
2023-11-03 00:12:02 -0400

    Update and delete on contact

--- server/routes/contactRouter.js
+++ server/routes/contactRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', contactController.getAllContacts);
 router.get('/:id', contactController.getContact, (req, res) => res.json(res.contact));
 router.post('/', contactController.createContact);
-
-// Add routes for updating, deleting, etc., as needed
+router.put('/:id', contactController.updateContact);
+router.delete('/:id', contactController.deleteContact);
 
 module.exports = router;
