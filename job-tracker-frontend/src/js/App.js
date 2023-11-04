e0d6f10dae32aeaca61116f99dc6ed66aa727e40
kimjun0206 <Jun.Kim@Dynata.com>
2023-11-04 14:27:53 -0700

    creating jobs and skills

--- job-tracker-frontend/src/js/App.js
+++ job-tracker-frontend/src/js/App.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import CreateUser from './CreateUser';
-import JobForm from './JobForm';
-import SkillForm from './SkillForm';
+import Jobs from './Jobs';
+import Skills from './Skills';
 import Contacts from './Contacts';
-import Login from './Login';  
-import Profile from './Profile'; // Import the new Profile component
+import Login from './Login';
+import Profile from './Profile';
 import '../css/App.css';
 
 function App() {
     return (
         <Router>
             <div className="app-container">
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/create-user">Create User</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>  {/* Link to the new Profile page */}
-                        <li><Link to="/create-job">Create Job</Link></li>
-                        <li><Link to="/create-skill">Create Skill</Link></li>
-                        <li><Link to="/contacts">Contacts</Link></li>
-                    </ul>
-                </nav>
 
                 <Routes>
-                    <Route path="/" element={<div>Welcome to the Job Tracker</div>} />
+                    <Route path="/" element={<Login />} />
                     <Route path="/create-user" element={<CreateUser />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/profile" element={<Profile />} />  {/* Add the Profile route */}
-                    <Route path="/create-job" element={<JobForm />} />
-                    <Route path="/create-skill" element={<SkillForm />} />
+                    {/* Redirecting /login to the home page */}
+                    <Route path="/login" element={<Navigate replace to="/" />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/jobs" element={<Jobs />} />
+                    <Route path="/skills" element={<Skills />} />
                     <Route path="/contacts" element={<Contacts />} />
+                    {/* Add more routes as needed */}
                 </Routes>
             </div>
         </Router>
---
+++ job-tracker-frontend/src/js/Jobs.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import '../css/Jobs.css';
+
+const Jobs = () => {
+    const [jobs, setJobs] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [formData, setFormData] = useState({
+        jobTitle: '',
+        jobLocation: '',
+        jobDescription: ''
+    });
+
+    // Fetch all jobs when the component mounts
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const response = await fetch('http://localhost:8001/api/jobs');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setJobs(data); // Assuming the API returns an array of jobs
+            } catch (error) {
+                console.error('There was a problem retrieving jobs data:', error);
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJobs();
+    }, []);
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newJob = {
+            title: formData.jobTitle,
+            location: formData.jobLocation,
+            description: formData.jobDescription
+        };
+
+        try {
+            const response = await fetch('http://localhost:8001/api/jobs', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newJob)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            setJobs([...jobs, result]);
+            setFormData({ jobTitle: '', jobLocation: '', jobDescription: '' }); // Reset form
+        } catch (error) {
+            console.error('There was a problem posting the new job:', error);
+            setError(error.message);
+        }
+    };
+
+    return (
+        <div className="form-container">
+            <h2>Create Job/Internship</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="jobTitle"
+                    placeholder="Job Title"
+                    value={formData.jobTitle}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="jobLocation"
+                    placeholder="Location"
+                    value={formData.jobLocation}
+                    onChange={handleChange}
+                    required
+                />
+                <textarea
+                    name="jobDescription"
+                    placeholder="Job Description"
+                    value={formData.jobDescription}
+                    onChange={handleChange}
+                    required
+                />
+                <button type="submit">Create Job</button>
+            </form>
+            {error && <p className="error">Error: {error}</p>}
+            {loading ? (
+                <p>Loading jobs...</p>
+            ) : (
+                jobs.length > 0 ? (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Location</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {jobs.map((job) => (
+                                <tr key={job._id}>
+                                    <td>{job.title}</td>
+                                    <td>{job.location}</td>
+                                    <td>{job.description}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No jobs found.</p>
+                )
+            )}
+        </div>
+    );
+}
+
+export default Jobs;
