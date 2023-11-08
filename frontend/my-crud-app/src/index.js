f5e1495a5f84bd2e0ae73b29315652ff65252afa
Yee <yeeei@oregonstate.edu>
2023-11-08 00:37:05 -0500

    Added Jobs and changed the Contacts, so I can post

--- frontend/my-crud-app/src/index.js
+++ frontend/my-crud-app/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'; // or any other stylesheets if you have them
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.render(
   <React.StrictMode>
+  <AuthProvider>
     <App />
-  </React.StrictMode>,
+  </AuthProvider>
+</React.StrictMode>,
   document.getElementById('root')
 );
