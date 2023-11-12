a169f9e618e00562d1158c382b53ab2f867caa55
kimjun0206 <Jun.Kim@Dynata.com>
2023-11-11 17:23:56 -0800

    update contacts.js to update delete record
    update contacts.js to update delete record

--- job-tracker-frontend/src/js/Contacts.js
+++ job-tracker-frontend/src/js/Contacts.js
@@ -7,11 +7,11 @@ const Contacts = () => {
     const [contacts, setContacts] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [editingId, setEditingId] = useState(null); // Track the ID of the contact being edited
+    const [formData, setFormData] = useState({}); // Form data for updates
 
-    // Extract userId from the location state
     const userId = location.state?.userId;
 
-    // Fetch contacts when component mounts or userId changes
     useEffect(() => {
         const fetchContacts = async () => {
             setLoading(true);
@@ -21,7 +21,7 @@ const Contacts = () => {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const data = await response.json();
-                setContacts(data.contacts); // <-- Update this line
+                setContacts(data.contacts);
             } catch (error) {
                 console.error('There was a problem retrieving contacts data:', error);
                 setError(error.message);
@@ -35,7 +35,6 @@ const Contacts = () => {
         }
     }, [userId]);
 
-    // Handle form submissions for new contacts
     const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
@@ -62,6 +61,59 @@ const Contacts = () => {
         }
     };
 
+    const handleEdit = (contact) => {
+        setEditingId(contact._id);
+        setFormData(contact); // Initialize form data with contact's current data
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleUpdateSubmit = async (event, id) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:8001/api/contacts/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const updatedContact = await response.json();
+            setContacts(contacts.map(contact => contact._id === id ? updatedContact : contact));
+            setEditingId(null); // Reset editing ID to null after update
+        } catch (error) {
+            console.error('There was a problem updating the contact:', error);
+            setError(error.message);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:8001/api/contacts/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            setContacts(contacts.filter(contact => contact._id !== id));
+        } catch (error) {
+            console.error('There was a problem deleting the contact:', error);
+            setError(error.message);
+        }
+    };
+
     return (
         <div className="form-container">
             {userId ? <h2>User ID: {userId}</h2> : <h2>Loading user...</h2>}
@@ -71,6 +123,7 @@ const Contacts = () => {
             ) : (
                 <>
                     <form onSubmit={handleSubmit}>
+                        {/* Form fields for adding a new contact */}
                         <input type="text" name="name" placeholder="Name" required />
                         <input type="email" name="email" placeholder="Email" required />
                         <input type="text" name="phoneNumber" placeholder="Phone Number" />
@@ -90,17 +143,41 @@ const Contacts = () => {
                                     <th>Company</th>
                                     <th>Job Title</th>
                                     <th>Notes</th>
+                                    <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {contacts.map((contact) => (
                                     <tr key={contact._id}>
-                                        <td>{contact.name}</td>
-                                        <td>{contact.email}</td>
-                                        <td>{contact.phoneNumber}</td>
-                                        <td>{contact.company}</td>
-                                        <td>{contact.jobTitle}</td>
-                                        <td>{contact.notes}</td>
+                                        {editingId === contact._id ? (
+                                            // Editable row
+                                            <>
+                                                <td><input type="text" name="name" value={formData.name} onChange={handleChange} /></td>
+                                                <td><input type="email" name="email" value={formData.email} onChange={handleChange} /></td>
+                                                <td><input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} /></td>
+                                                <td><input type="text" name="company" value={formData.company} onChange={handleChange} /></td>
+                                                <td><input type="text" name="jobTitle" value={formData.jobTitle} onChange={handleChange} /></td>
+                                                <td><textarea name="notes" value={formData.notes} onChange={handleChange}></textarea></td>
+                                                <td>
+                                                    <button onClick={(e) => handleUpdateSubmit(e, contact._id)}>Save</button>
+                                                    <button onClick={() => setEditingId(null)}>Cancel</button>
+                                                </td>
+                                            </>
+                                        ) : (
+                                            // Display row
+                                            <>
+                                                <td>{contact.name}</td>
+                                                <td>{contact.email}</td>
+                                                <td>{contact.phoneNumber}</td>
+                                                <td>{contact.company}</td>
+                                                <td>{contact.jobTitle}</td>
+                                                <td>{contact.notes}</td>
+                                                <td>
+                                                    <button onClick={() => handleEdit(contact)}>Edit</button>
+                                                    <button onClick={() => handleDelete(contact._id)}>Delete</button>
+                                                </td>
+                                            </>
+                                        )}
                                     </tr>
                                 ))}
                             </tbody>
